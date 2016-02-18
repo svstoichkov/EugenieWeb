407d4d123b946a977ae4ce97d139f4ff1fec3885
svstoichkov <sv.stoichkov@yahoo.com>
2016-02-18 17:02:59 +0200

    Added landing page

--- Source/Web/EugenieWeb.Web/App_Start/BundleConfig.cs
+++ Source/Web/EugenieWeb.Web/App_Start/BundleConfig.cs
@@ -16,12 +16,18 @@
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
-            bundles.Add(new ScriptBundle("~/bundles/pages").Include("~/Scripts/pages.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/landing/loader").Include("~/Scripts/Landing/loader.js"));
+            bundles.Add(new ScriptBundle("~/bundles/landing/main").Include("~/Scripts/Landing/main.js"));
+            bundles.Add(new ScriptBundle("~/bundles/landing/modernizr").Include("~/Scripts/Landing/modernizr.custom.js"));
+            bundles.Add(new ScriptBundle("~/bundles/landing/vendor").Include("~/Scripts/Landing/vendor.js"));
+
+            // bundles.Add(new ScriptBundle("~/bundles/pages").Include("~/Scripts/pages.min.js"));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css", "~/Content/pages.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css", "~/Content/pages-icons.css", "~/Content/pages.css"));
+            bundles.Add(new StyleBundle("~/Content/Landing/css").Include("~/Content/Landing/material-design-iconic-font.min.css", "~/Content/Landing/bootstrap.css", "~/Content/Landing/main.css"));
         }
     }
 }
---
+++ Source/Web/EugenieWeb.Web/Areas/Management/ManagementAreaRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace EugenieWeb.Web.Areas.Management
+{
+    public class ManagementAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName => "Management";
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Management_default",
+                "Management/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
